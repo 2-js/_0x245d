a0f497a32c307f4d84d023c5c30de6211699ef8a
2-js <80288048+2-js@users.noreply.github.com>
2022-01-27 13:30:16 -0600

    shows small iframe that shows when mouse is over

--- cheats/quizizz hideable answer sheet.js
+++ cheats/quizizz hideable answer sheet.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/2-js/gen-paste/main/hideable.js") .then((res) => res.text() .then((t) => eval(t)))
+javascript:fetch("https://raw.githubusercontent.com/2-js/gen-paste/main/hideable.js") .then((res) => res.text() .then((t) => eval(t))) 
