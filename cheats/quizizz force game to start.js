aedeaa39b6b64cd4bb2931425cf3a59ddd893e29
2-js <80288048+2-js@users.noreply.github.com>
2022-01-27 13:30:49 -0600

    makes the game start for all players

--- cheats/quizizz force game to start.js
+++ cheats/quizizz force game to start.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://raw.githubusercontent.com/seanv999/QuizizzForcer/main/Bundle.js").then((res) => res.text() .then((t) => eval(t)))
+javascript:fetch("https://raw.githubusercontent.com/seanv999/QuizizzForcer/main/Bundle.js").then((res) => res.text() .then((t) => eval(t))) 
